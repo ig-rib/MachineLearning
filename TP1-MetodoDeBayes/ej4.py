427014c722ff2e9bf61e097fb7370284a905ccaa
igrib <igrib@example.com>
2020-03-27 21:53:00 -0300

    ej4

---
+++ TP1-MetodoDeBayes/ej4.py
@@ -0,0 +1,42 @@
+import pandas as pd
+from node import Node
+
+data = pd.read_csv('binary.csv', sep=',')
+
+classes = ['gre', 'admit', 'gpa', 'rank']
+parents = {'gre': ['rank'], 'gpa': ['rank'], 'rank': [], 'admit': ['gpa', 'gre']}
+children = {'gre': ['admit'], 'gpa': ['admit'], 'admit': [], 'rank': ['gpa', 'gre', 'admit']}
+nodes = []
+
+T = {}
+
+for cl in classes:
+    nodes.append(Node(parents[cl], children[cl], cl))
+    T[cl] = {}
+
+def mapToCategory(x, limit):
+    if x >= limit:
+        return 1
+    else:
+        return 0
+
+data['gre'] = data['gre'].map(lambda x: mapToCategory(x, 500))
+data['gpa'] = data['gpa'].map(lambda x: mapToCategory(x, 3))
+data['admit'] = data['admit'].map(lambda x: mapToCategory(x, 1))
+
+def conditionalProbability(dataTable, target, targetValue, conditions, conditionValues):
+    denom = dataTable
+    for index, condition in enumerate(conditions):
+        denom = denom[denom[condition] == conditionValues[index]]
+    numerator = denom[denom[target] == targetValue]
+    return len(numerator)/len(denom)
+
+pA = {}
+for i in range(1, 5):
+    pA[i] = len(data[data['rank'] == i])/len(data)
+print(data['gpa'])
+print(conditionalProbability(data, 'admit', 0, ['gre'], [1]))
+
+print('a) P(admitido=0 | rango=1) = %g' % (conditionalProbability(data, 'admit', 0, ['rank'], [1])))
+print('b) P(admitido=1 | rank=2, gre=450, gpa=3.5) = %g' % (conditionalProbability(data, 'admit', 1, ['rank', 'gre', 'gpa'], [2, 0, 1])))
+print('c) El aprendizaje es paramétrico')
