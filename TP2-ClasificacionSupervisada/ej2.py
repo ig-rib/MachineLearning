6239f0e9c028d78c4dab6a75b9ac998872ae35f9
mscomazzon <mscomazzon@itba.edu.ar>
2020-04-14 12:35:54 -0300

    2.a & 2.b

---
+++ TP2-ClasificacionSupervisada/ej2.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+data = pd.read_csv('reviews_sentiment.csv', sep=';')
+
+
+percentage = 0.50
+splittingIndex = int(len(data)*percentage)
+training = data[:splittingIndex]
+testSet = data[splittingIndex:]
+
+word_count = 0
+count = 0
+
+for i in range(len(data)):
+	star_rating = data.iloc[i]["Star Rating"]
+	if(star_rating==1):
+		word_count += data.iloc[i]["wordcount"]
+		count+=1
+average = word_count/count
+print("b) Los reviews valuados con 1 estrella tienen en promedio " + str(average) + " palabras")
+
