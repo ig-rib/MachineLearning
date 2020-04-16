9163eaa620d3f1de20b25e3f1f01658b46f1caa7
igrib <igrib@example.com>
2020-04-16 20:15:00 -0300

    more svm

--- TP3-SVM/ej2/ej2.py
+++ TP3-SVM/ej2/ej2.py
@@ -1,10 +1,14 @@
 #!/bin/python3
 
 import pandas as pd
+from sklearn import svm
+import random as rd
 
 data = pd.read_excel('TP3-SVM/data/acath.xls')
-data
-
+data = data.sample(frac=1)
+choleste = 'choleste'
+# data[choleste] = data[choleste].fillna(data[choleste].mean())
+data.apply(lambda x: x.fillna(x.mean()),axis=0)
 # a)
 
 percentage = 0.5
@@ -12,5 +16,21 @@ splittingIndex = int(percentage * len(data))
 training = data[:splittingIndex]
 testSet = data[splittingIndex:]
 
+
 # b)
 
+objective = 'sigdz'
+
+trainingVectors = training.loc[:, training.columns != objective]
+trainingYs = training[objective]
+
+## possible kernel values are 'linear', 'poly', 'rbf', 'sigmoid', 'precomputed'
+
+suppVectorMachine = svm.SVC(C = 1.0, kernel = 'linear')
+suppVectorMachine.fit(trainingVectors, trainingYs)
+
+testVectors = testSet.loc[:, testSet.columns != objective]
+testYs = testSet[objective]
+
+predicted = suppVectorMachine.predict(testVectors)
+predicted
