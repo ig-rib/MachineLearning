fb68ed7bd8b7a9aec022a2d97568535e3c78f38e
igrib <igrib@example.com>
2020-04-20 21:37:59 -0300

    3.2

--- TP3-SVM/ej2/ej2.py
+++ TP3-SVM/ej2/ej2.py
@@ -4,6 +4,7 @@ import pandas as pd
 from sklearn import svm
 import random as rd
 import numpy as np
+import matplotlib.pyplot as plt
 
 data = pd.read_excel('TP3-SVM/data/acath.xls')
 data = data.sample(frac=1)
@@ -12,7 +13,7 @@ choleste = 'choleste'
 data = data.apply(lambda x: x.fillna(x.mean()),axis=0)
 # a)
 
-percentage = 0.5
+percentage = 0.75
 splittingIndex = int(percentage * len(data))
 training = data[:splittingIndex]
 testSet = data[splittingIndex:]
@@ -41,11 +42,30 @@ TNs = 0
 for i in range(len(testVectors)):
     predictedValue = suppVectorMachine.predict(np.array(testVectors.iloc[i]).reshape(1, -1))[0]
     actualValue = testYs.iloc[i]
-    if predictedValue == 0: 
+    if predictedValue == 1: 
         totalClassifiedAsPos += 1
         if predictedValue == actualValue:
             TPs += 1
-    else: 
+    elif predictedValue == 0: 
         totalClassifiedAsNeg += 1
         if predictedValue == actualValue:
             TNs += 1
+
+def plotConfusionMatrix(testSet, TPs, TNs, title, figNo):
+    matrix = []
+    matrix.append([TPs, len(testSet[testSet == 1]) - TPs])
+    matrix.append([len(testSet[testSet == 0]) - TNs, TNs])
+    print(matrix)
+    fig, ax = plt.subplots()
+    ax.matshow(matrix)
+    for (i, j), z in np.ndenumerate(matrix):
+        ax.text(j, i, '{:0.1f}'.format(z), ha='center', va='center',
+                bbox=dict(boxstyle='round', facecolor='white', edgecolor='0.1'))
+    ax.set_xticklabels(['', 'Positives', 'Negatives'])
+    ax.set_yticklabels(['', 'Positives', 'Negatives'])
+    plt.title(title)
+    plt.show()
+
+plotConfusionMatrix(testYs, TPs, TNs, 'Confusion Matrix for SVM', 1)
+
+# c)
