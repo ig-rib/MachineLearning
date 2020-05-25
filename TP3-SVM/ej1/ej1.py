5f99426e2987c7641ead1f498a04f2133c300c1a
igrib <igrib@example.com>
2020-05-25 19:29:35 -0300

    finding optimal hyperplane from NN-calculated line

--- TP3-SVM/ej1/ej1.py
+++ TP3-SVM/ej1/ej1.py
@@ -4,17 +4,18 @@ import random as rd
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
+import geoUtils as gU
 
 points = []
-for i in range(1000):
+for i in range(30):
     points.append((rd.random()*5, rd.random()*5))
 
 def f(x):
     return 1.5 * x + 0.25
 
 def mapToClass(pair):
-    if pair[1] > f(pair[0]): return 1
-    else: return -1
+    if pair[1] > f(pair[0]): return -1
+    else: return 1
 
 def mapToWrongClass(pair):
     if pair[1] > f(pair[0]): return -1
@@ -66,19 +67,44 @@ def classifyAndTest(D, epochs=1000):
     plt.ylim(0, 5)
     plt.xlim(0, 5)
     plt.show()
+    return perceptron
 
 print('\n######################################################\nCORRECTLY CLASSIFIED TRAINING SET\n######################################################\n')
-classifyAndTest(D)
+perceptron = classifyAndTest(D)
 
-points = []
-for i in range(1000):
-    points.append((rd.random()*5, rd.random()*5))
-D = [ [p, mapToClass(p)] for p in points]
-points = []
-for i in range(100):
-    x = rd.random()*5
-    points.append((x, f(x) - rd.random()*0.1 * (-1 if rd.random() < 0.5 else 1)))
-D.extend([[p, mapToWrongClass(p)] for p in points])
+clA, clB = gU.getNClosest(D, perceptron.w, 3)
 
-print('\n######################################################\nTRAINING SET WITH SOME INCORRECTLY CLASSIFIED EXAMPLES\n######################################################\n')
-classifyAndTest(D, epochs=250)
+
+
+bestHyp, hyps = gU.getBestHyperplane(clA, clB)
+for hyp in [bestHyp]:
+    red = [x[0] for x in D if x[1] == -1 and perceptron.classify(x[0]) == x[1]]
+    blue = [x[0] for x in D if x[1] == 1 and perceptron.classify(x[0]) == x[1]]
+    plt.scatter([r[0] for r in red], [r[1] for r in red], color='red')
+    plt.scatter([b[0] for b in blue], [b[1] for b in blue], color='blue')
+    plt.scatter([r[0][0] for r in clA], [r[0][1] for r in clA], color='green')
+    plt.scatter([b[0][0] for b in clB], [b[0][1] for b in clB], color='orange')
+    x = np.linspace(0, 5, 100)
+    slope = -perceptron.w[1]/perceptron.w[2]
+    intercept = perceptron.w[0]/perceptron.w[2]
+    print(f'\nSeparating Line Equation:\n{slope}*x + {intercept}\n\n')
+    y = [ xi*slope + intercept for xi in x ]
+    plt.plot(x, y)
+    y = [ xi*hyp['m'] + hyp['b'] for xi in x ]
+    plt.plot(x, y)
+    print(f'\nSeparating Line Equation:\n{hyp["m"]}*x + {hyp["b"]}\n\n')
+    plt.ylim(0, 5)
+    plt.xlim(0, 5)
+    plt.show()
+# points = []
+# for i in range(1000):
+#     points.append((rd.random()*5, rd.random()*5))
+# D = [ [p, mapToClass(p)] for p in points]
+# points = []
+# for i in range(100):
+#     x = rd.random()*5
+#     points.append((x, f(x) - rd.random()*0.1 * (-1 if rd.random() < 0.5 else 1)))
+# D.extend([[p, mapToWrongClass(p)] for p in points])
+
+# print('\n######################################################\nTRAINING SET WITH SOME INCORRECTLY CLASSIFIED EXAMPLES\n######################################################\n')
+# classifyAndTest(D, epochs=250)
