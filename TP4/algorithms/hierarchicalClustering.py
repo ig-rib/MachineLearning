200a91de2c1a14c4c29ee8b54eea6f11661c80a1
Ignacio Ribas <igrib@example.com>
2020-06-22 15:58:58 -0300

    [Hierarchical Clustering] Faster version (still slow for large samples)

--- TP4/algorithms/hierarchicalClustering.py
+++ TP4/algorithms/hierarchicalClustering.py
@@ -9,8 +9,8 @@ class ClusterNode:
     ## Get max distance between any 2 examples from
     # different clusters, between this and another
     # cluster 
-    def getMaxDistance(self, other, D):
-        return max([max([np.linalg.norm(D[i] - D[j]) for j in other.nodes]) for i in self.nodes])
+    def getMaxDistance(self, other, matrix):
+        return max([max([matrix.item((i, j)) for j in other.nodes]) for i in self.nodes])
 
     def __repr__(self):
         return f"ClusterNode {self.nodes}" # % (self.nodes)
@@ -27,33 +27,33 @@ class HierarchicalClustering:
 
     def group(self, D):
         groups = [ClusterNode(nodes=[i]) for i in range(len(D))]
-
+        distMatrix = np.matrix([[np.linalg.norm(D[i] - D[j]) for j in range(len(D))] for i in range(len(D))])
         ## as long as there are more than one groups left
         while len(groups) > 1:
-            distances = [ (x, *min([(x.getMaxDistance(other, D), other) for other in groups if other != x], key=lambda x: x[0])) for x in groups]
-            minPair = min(distances, key=lambda x: x[1])
-            newNode = ClusterNode(children=[minPair[0], minPair[2]], nodes=[*minPair[0].nodes, *minPair[2].nodes])
+            distances = [ (x, min([(x.getMaxDistance(other, distMatrix), other) for other in groups if other != x], key=lambda x: x[0])) for x in groups]
+            minPair = min(distances, key=lambda x: x[1][0])
+            newNode = ClusterNode(children=[minPair[0], minPair[1][1]], nodes=[*minPair[0].nodes, *minPair[1][1].nodes])
             minPair[0].p = newNode
-            minPair[2].p = newNode
+            minPair[1][1].p = newNode
             groups.remove(minPair[0])
-            groups.remove(minPair[2])
+            groups.remove(minPair[1][1])
             groups.append(newNode)
         
         return newNode
 
-D = np.matrix([
-    [0, 1, 2],
-    [3, 4, 5],
-    [10, 7, 8],
-    [1, 5, 2],
-    [10, 4, 1]
-])
+# D = np.matrix([
+#     [0, 1, 2],
+#     [3, 4, 5],
+#     [10, 7, 8],
+#     [1, 5, 2],
+#     [10, 4, 1]
+# ])
 
-distMatrix = [[np.linalg.norm(D[i] - D[j]) for j in range(len(D))] for i in range(len(D))]
-for i in range(len(D)):
-    print(distMatrix[i])
+# distMatrix = [[np.linalg.norm(D[i] - D[j]) for j in range(len(D))] for i in range(len(D))]
+# for i in range(len(D)):
+#     print(distMatrix[i])
 
-print(np.linalg.norm(D[0] - D[1]))
+# print(np.linalg.norm(D[0] - D[1]))
 
-hc = HierarchicalClustering()
-hc.group(D)
+# hc = HierarchicalClustering()
+# hc.group(D)
--- TP4/main.py
+++ TP4/main.py
@@ -60,43 +60,43 @@ data = file_data[['age', 'cad.dur', 'choleste']]
 label = file_data['sigdz'].to_list()
 
 # normalize data
+data = normalize_data(data)
+data = data.to_numpy()
+
+# from sklearn.model_selection import train_test_split
+train_percentage = 0.8
+train_data, test_data, train_labels, test_labels = train_test_split(data, label, train_size=train_percentage)
+
+print('Ejercicio B, sin sex')
+coefficients, intercept = logistic_training('b', train_data, test_data, train_labels, test_labels)
+
+p_num_exp = intercept[0] + coefficients[0][0]*60 + coefficients[0][1]*2 + coefficients[0][2]*199
+p_num = pow(math.e, p_num_exp);
+p_den = 1 + p_num
+p = p_num/p_den
+print("Ejercicio C: La probabilidad de que tenga la enfermedad es: " + str(p) + " como p>0.5 esta enfermo\n")
+
+# d) tenemos que agregar el sexo y hacemos el mismo procedimiento que (a)
+data = file_data[['sex', 'age', 'cad.dur', 'choleste']]
 #data = normalize_data(data)
-# data = data.to_numpy()
+data = data.to_numpy()
 
-# # from sklearn.model_selection import train_test_split
-# train_percentage = 0.8
-# train_data, test_data, train_labels, test_labels = train_test_split(data, label, train_size=train_percentage)
+train_percentage = 0.8
+train_data, test_data, train_labels, test_labels = train_test_split(data, label, train_size=train_percentage)
 
-# print('Ejercicio B, sin sex')
-# coefficients, intercept = logistic_training('b', train_data, test_data, train_labels, test_labels)
-
-# p_num_exp = intercept[0] + coefficients[0][0]*60 + coefficients[0][1]*2 + coefficients[0][2]*199
-# p_num = pow(math.e, p_num_exp);
-# p_den = 1 + p_num
-# p = p_num/p_den
-# print("Ejercicio C: La probabilidad de que tenga la enfermedad es: " + str(p) + " como p>0.5 esta enfermo\n")
-
-# # d) tenemos que agregar el sexo y hacemos el mismo procedimiento que (a)
-# data = file_data[['sex', 'age', 'cad.dur', 'choleste']]
-# #data = normalize_data(data)
-# data = data.to_numpy()
-
-# train_percentage = 0.8
-# train_data, test_data, train_labels, test_labels = train_test_split(data, label, train_size=train_percentage)
-
-# print('Ejercicio D, con sex')
-# logistic_training('d', train_data, test_data, train_labels, test_labels)
+print('Ejercicio D, con sex')
+logistic_training('d', train_data, test_data, train_labels, test_labels)
 
 # e) 
 
 data = file_data[['age', 'choleste', 'cad.dur']]
 scaler = StandardScaler()
 data1 = pd.DataFrame(scaler.fit_transform(data), index=data.index, columns=data.columns)
-data1 = np.matrix(data1.to_numpy())
+data1 = data1.to_numpy()
 
-train_percentage = 0.8
+train_percentage = 0.1
 train_data, test_data, train_labels, test_labels = train_test_split(data1, label, train_size=train_percentage)
 
 hc = HierarchicalClustering()
-root = hc.group(data1)
+root = hc.group(np.matrix(train_data))
 root
